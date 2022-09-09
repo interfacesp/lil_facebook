2889e5c07ac464ef648b6f2117628fc270ac59b2
Espoir Gahungere <espoir.gahungere@interface3.be>
2022-09-09 16:22:20 +0200

    message erreur

--- index.php
+++ index.php
@@ -19,8 +19,11 @@
          */
         $isFound = false; //connu
         $connectedUser;
+        $loginAttempt = 0;
 
         if(isset($_POST['username'], $_POST['password'])){
+            
+            $loginAttempt++;
 
             $user_name = $_POST['username'];
             $pass_word = $_POST['password'];
@@ -39,17 +42,24 @@
                 $isFound = true;
                 $connectedUser = $result[0]['username'];
                 $email = $result[0]['email'];
-
             }
-            
 
         }
 
         if($isFound){
             echo "Bienvenue sur page d'accueil ! $connectedUser ($email)";
         }else {
+
+
+            
             include_once('views/login.php');
 
+
+            if(!$isFound && $loginAttempt >=1 ){
+                echo "Username and password incorrect! ";
+                $loginAttempt = 0;
+            }
+            
         }
 
     ?>
